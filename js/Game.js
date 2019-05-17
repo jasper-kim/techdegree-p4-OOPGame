02271dab119a3a60e489b541bb8a54a860b86d3a
Jasper Kim <yjikim0219@gmail.com>
2019-05-17 15:22:59 -0700

    Add Classes and EventListener
    app.js:
    - Add eventlisteners to start button and onscreen keyboard
    
    Game.js:
    - Add constrcution method
    - Add methods to use in the game(code inside of the methods not completed)
    
    Phrase.js:
    - Add constrcution method
    - Add methods to use in the game(code inside of the methods not completed)

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,24 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = this.createPhrases();
+        this.activePhrase = null;
+    }
+
+    startGame() {}
+
+    gameOver() {}
+
+    handleInteraction() {}
+
+    createPhrases() {}
+
+    getRandomPhrase() {}
+
+    checkForWin() {}
+
+    removeLife() {}
+}
--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+document.getElementById('btn__reset').addEventListener('click', function() {
+
+});
+document.getElementById('qwerty').addEventListener('click', function() {
+
+});
