be06906e32ee30da3fecab8fd504ba22c0e3fb60
Jasper Kim <yjikim0219@gmail.com>
2019-05-17 16:19:45 -0700

    Updated addPhraseToDisplay() in Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -6,7 +6,33 @@ class Phrase {
         this.phrase = phrase.toLowerCase();
     }
 
-    addPhraseToDisplay() {}
+    /**
+    * Display phrase on game board
+    */
+    addPhraseToDisplay() {
+        const chars = this.phrase.split('');
+        const patt = /[a-z]/;
+
+        for(let char of chars) {
+            let result = patt.test(char);
+            if(result) {
+                let li = document.createElement('li');
+
+
+                li.textContent = char;
+                li.classList.add('hide', 'letter', char);
+
+                document.querySelector('#phrase ul').appendChild(li);
+            } else {
+                let li = document.createElement('li');
+
+                li.textContent = char;
+                li.className = 'space';
+
+                document.querySelector('#phrase ul').appendChild(li);
+            }
+        }
+    }
 
     checkLetter() {}
 
