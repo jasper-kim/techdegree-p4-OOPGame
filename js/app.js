0e0832fc0ecbd1e060e9a789975eae6247e7852e
Jasper Kim <yjikim0219@gmail.com>
2019-05-18 00:52:46 -0700

    Update app.js
    Add resetGame() function

--- js/app.js
+++ js/app.js
@@ -1,9 +1,41 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
-let game;
+let game = null;
+
+/**
+ * Reset gameboard between games
+ * @param {object} game - Game object to used in game
+ */
+function resetGame(game){
+    if(game !== null) {
+        //Remove all `li` elements from the Phrase `ul` element
+        const ul = document.querySelector('ul');
+        while(ul.firstChild) {
+            ul.removeChild(ul.firstChild);
+        }
+
+        //Enable all of the onscreen keyboard buttons and remove classes from it
+        const keys = document.getElementsByClassName('key');
+        for(let key of keys) {
+            key.removeAttribute('disabled');
+            key.classList.remove('chosen', 'wrong');
+        }
+        
+        //Reset all of the heart images
+        const imgs = document.getElementsByTagName('img');
+        for(let img of imgs) {
+            img.setAttribute('src', 'images/liveHeart.png');
+        }
+
+        //Reset overlay Element class to start
+        document.getElementById('overlay').className = 'start';
+    }
+}
 
 document.getElementById('btn__reset').addEventListener('click', function() {
+    resetGame();
+    
     game = new Game();
     game.startGame();
 });
